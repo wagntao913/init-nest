81945453fca269073f1797432f5783c997f19b7c
wagntao913 <forestwt@126.com>
2020-08-28 15:29:32 +0800

    增加日志管理功能

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as express from 'express';
+
+import { logger } from './middleware/logger.middleware';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 跨域设置
   app.enableCors();
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  // 监听路由，打印日志
+  app.use(logger);
+  app.useGlobalInterceptors(new TransformInterceptor())
+  
 
   const options = new DocumentBuilder()
     .setTitle('nest 接口文档')
